b7eea7c43004dd54aa0fa573ef8ead36bbcf083d
Jef Wagner <jefwagner@gmail.com>
2023-04-01 07:07:25 -0500

    changed from setup.py to pyproject.toml, for real this time

---
+++ _setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## @file setup.py Python/Numpy interface for flints
+"""\
+This package creates a rounded floating point interval (flint) type in python
+and extends the type to numpy allowing for the creation and manipulation of
+arrays of flints. All the arithmetic operations as well as a few elementary
+mathematical functions are enabled for them. The core code is written in c...
+because 'why not?'
+"""
+# Copyright (c) 2023, Jef Wagner <jefwagner@gmail.com>
+# This file is part of numpy-flint.
+#
+# Numpy-flint is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Numpy-flint is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# numpy-flint. If not, see <https://www.gnu.org/licenses/>.
+
+from setuptools import Extension, setup
+import numpy as np
+
+# 0.0.X -> Not all functionality has been implemented
+# 0.X.Y -> 'Most' functionality has been implemented and is ready for testing
+# X.Y.Z -> A stable and tested version is ready for release
+version = '0.1.0'
+
+extra_compiler_args = []
+
+extensions = [
+    Extension(
+        name='flint.numpy_flint',
+        sources=['src/numpy_flint.c'],
+        depends=[
+            'src/flint.h',
+            'src/numpy_flint.c',
+        ],
+        include_dirs=[np.get_include(),'src'],
+    )
+]
+
+setup_metadata = dict(
+    name='numpy-flint', # Name of the package on pypi
+    packages=['flint'], # Actual package name as used in python
+    # package_dir = {'':'.'},
+    url='https://github.com/jefwagner/numpy-flint',
+    author="Jef Wagner",
+    author_email="jefwagner@gmail.com",
+    description="Add a rounded floating point interval (flint) dtype to NumPy",
+    long_description=__doc__,
+    ext_modules=extensions,
+    install_requires=["numpy>=1.22"],
+    extras_require={},
+    version=version,
+)
+
+def build(setup_kwargs):
+    setup_kwargs.update({"ext_modules": extensions})
+
+if __name__=='__main__':
+    setup(**setup_metadata)
