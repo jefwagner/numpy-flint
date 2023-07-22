d9523438f0d091abd02c36db6fb5448941291a47
Jef Wagner <jefwagner@gmail.com>
2023-07-22 17:36:48 -0500

    added 'get_include' method

--- setup.py
+++ setup.py
@@ -30,13 +30,13 @@ import numpy as np
 setup_args = dict(
     ext_modules = [
         Extension(
-            name='flint',
-            sources=['src/numpy_flint.c'],
+            name='flint.flint',
+            sources=['src/flint/numpy_flint.c'],
             depends=[
-                'src/flint.h',
-                'src/numpy_flint.c',
+                'src/flint/flint.h',
+                'src/flint/numpy_flint.c',
             ],
-            include_dirs=[np.get_include(),'src'],
+            include_dirs=[np.get_include(),'src/flint'],
         )
     ]
 )
